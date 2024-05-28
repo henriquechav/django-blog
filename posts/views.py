9c165f3e8ed8f7b7d6388844d52a02f75b3dc3cb
Henrique Chaves <henriquelopes0611@gmail.com>
2024-05-28 10:13:17 -0300

    add `post.html` template

--- posts/views.py
+++ posts/views.py
@@ -5,3 +5,8 @@ def index(request):
     posts = Posts.objects.all()
     data = {'posts': posts}
     return render(request, 'index.html', data)
+
+def post(request, pk):
+    post = Posts.objects.get(id=pk)
+    data = {'post': post}
+    return render(request, 'post.html', data)
